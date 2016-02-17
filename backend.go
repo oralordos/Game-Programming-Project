7db418d924dd969358ba9e5414b65b71fa46d861
Shenung <shenung@mail.fresnostate.edu>
2016-02-17 13:09:09 -0800

    the backend go routine template

---
+++ backend.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/Oralordos/Game-Programming-Project/events"
+)
+
+func main() {
+	var inChn = make(chan events.Event)
+	go func() {
+		select {
+		case todo := <-inChn:
+			fmt.Println("Something")
+			fmt.Println(todo)
+		case <-time.After(time.Millisecond * 500):
+			fmt.Println("timeout")
+		}
+	}()
+}
