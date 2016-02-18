2a892e01f173ca5f720d35a24d6409152cdc7310
Shenung <shenung@mail.fresnostate.edu>
2016-02-18 10:36:09 -0800

    shanged timeouts

--- backend.go
+++ backend.go
@@ -7,19 +7,23 @@ import (
 	"github.com/Oralordos/Game-Programming-Project/events"
 )
 
+const frameDelta = 33 * time.Millisecond
+
 func init() {
 	go backend()
 }
 
 func backend() {
 	inChn := make(chan events.Event)
+	eta := time.Now()
 	for {
 		select {
 		case todo := <-inChn:
 			fmt.Println("Something")
 			fmt.Println(todo)
-		case <-time.After(time.Millisecond * 500):
+		case <-time.After(eta.Sub(time.Now())):
 			fmt.Println("timeout")
+			eta = time.Now().Add(frameDelta)
 		}
 	}
 }
