711fd9bae1c20365c957d9868db1b5221b5c1b29
Shenung <shenung@mail.fresnostate.edu>
2016-02-18 10:25:01 -0800

    inf loop

--- backend.go
+++ backend.go
@@ -13,11 +13,13 @@ func init() {
 
 func backend() {
 	inChn := make(chan events.Event)
-	select {
-	case todo := <-inChn:
-		fmt.Println("Something")
-		fmt.Println(todo)
-	case <-time.After(time.Millisecond * 500):
-		fmt.Println("timeout")
+	for {
+		select {
+		case todo := <-inChn:
+			fmt.Println("Something")
+			fmt.Println(todo)
+		case <-time.After(time.Millisecond * 500):
+			fmt.Println("timeout")
+		}
 	}
 }
