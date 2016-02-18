6d25d378717c4f04e979cda02ca4ef00011df0b3
Daniel <oralordos@gmail.com>
2016-02-18 09:35:48 -0800

    Finished events interface

--- events/events.go
+++ events/events.go
@@ -16,6 +16,8 @@ type EventManager struct {
 	frontOuts  []chan<- Event
 }
 
+var DefaultEventManager = NewEventManager()
+
 func NewEventManager() *EventManager {
 	return &EventManager{
 		input:      make(chan Event),
@@ -33,8 +35,39 @@ func (e *EventManager) AddListener(listener chan<- Event, direction int) {
 	}
 }
 
+func (e *EventManager) RemoveListener(listener chan<- Event, direction int) {
+	if direction&DirSystem == DirSystem {
+		for i, v := range e.systemOuts {
+			if v == listener {
+				e.systemOuts = append(e.systemOuts[:i], e.systemOuts[i+1:]...)
+				break
+			}
+		}
+	}
+	if direction&DirFront == DirFront {
+		for i, v := range e.frontOuts {
+			if v == listener {
+				e.frontOuts = append(e.frontOuts[:i], e.frontOuts[i+1:]...)
+				break
+			}
+		}
+	}
+}
+
 func (e *EventManager) SendEvent(event Event) {
 	go func() {
 		e.input <- event
 	}()
+}
+
+func AddListener(listener chan<- Event, direction int) {
+	DefaultEventManager.AddListener(listener, direction)
+}
+
+func RemoveListener(listener chan<- Event, direction int) {
+	DefaultEventManager.RemoveListener(listener, direction)
+}
+
+func SendEvent(event Event) {
+	DefaultEventManager.SendEvent(event)
 }
