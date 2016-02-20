81bc2e3c695214ddba02a37a73e508ef66cbe87a
Shenung <shenung@mail.fresnostate.edu>
2016-02-19 16:13:14 -0800

    made changes to inf loop and timeouts

--- backend.go
+++ backend.go
@@ -15,15 +15,16 @@ func init() {
 
 func backend() {
 	inChn := make(chan events.Event)
-	eta := time.Now()
+	// eta := time.Now()
+	eta := time.After(frameDelta)
 	for {
 		select {
 		case todo := <-inChn:
 			fmt.Println("Something")
 			fmt.Println(todo)
-		case <-time.After(eta.Sub(time.Now())):
+		case <-eta:
 			fmt.Println("timeout")
-			eta = time.Now().Add(frameDelta)
+			eta = time.After(frameDelta)
 		}
 	}
 }
